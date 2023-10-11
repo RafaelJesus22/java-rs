bb03034377e444617125c3a920d0e589f7284e60
Rafael Moura <rafael.moura@jazztech.com.br>
2023-10-11 09:26:55 -0300

    feat: created tass controller, repository and post request to save task

---
+++ src/main/java/br/com/rjcm/todolist/task/ITaskRepository.java
@@ -0,0 +1,6 @@
+package br.com.rjcm.todolist.task;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {}
