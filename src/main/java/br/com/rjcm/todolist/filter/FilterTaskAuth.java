75f6a3456b30f5928471bdfae0c2633ef904e71c
Rafael Moura <rafael.moura@jazztech.com.br>
2023-10-13 08:57:10 -0300

    feat: getting userId from request

--- src/main/java/br/com/rjcm/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rjcm/todolist/filter/FilterTaskAuth.java
@@ -50,6 +50,8 @@ public class FilterTaskAuth extends OncePerRequestFilter {
           .verify(password.toCharArray(), user.getPassword());
 
         if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+
           filterChain.doFilter(request, response);
         } else {
           response.sendError(401);
--- src/main/java/br/com/rjcm/todolist/task/TaskController.java
+++ src/main/java/br/com/rjcm/todolist/task/TaskController.java
@@ -1,10 +1,14 @@
 package br.com.rjcm.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,9 +19,11 @@ public class TaskController {
 
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
+  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
     this.taskRepository.save(taskModel);
-    System.out.println("Chegou no controller");
 
     return taskModel;
   }
